e41b94d45b30d1cae38f3eaf70790838897570ed
sandro-96 <154776732+sandro-96@users.noreply.github.com>
2024-02-26 00:33:18 +0700

    fix bug

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,24 @@
 import "./index.scss"
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Home = () => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        loadData()
+    }, []);
+    const loadData = () => {
+        const instance = axios.create({
+            withCredentials: true,
+            baseURL: 'http://localhost:8080/'
+        })
+        instance.get('group/list').then(value => {
+            console.log('dsasdsasdasd')
+        }).catch(reason => {
+            console.log(reason)
+        })
+    }
     return (
         <div>
             Home Page
