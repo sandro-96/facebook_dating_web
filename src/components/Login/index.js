588a88a46acc52f22ad40c43e68c837d872be668
sandro <sandro.vnib@gmail.com>
2024-03-04 11:39:20 +0700

    fix bug

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,24 +1,16 @@
 import "./index.scss"
-import { useForm } from "react-hook-form";
 import GoogleLogin from './GoogleLogin';
 import {useEffect} from "react";
 
 const Login = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => {
-        console.log(data);
-    };
     useEffect(() => {
-        if (localStorage.getItem('Authorization')) console.log(localStorage.getItem('Authorization'))
     }, []);
     return (
         <div className="login-wrap">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h2 className="mb-5">Đăng nhập</h2>
-                <div className="lp-button-group">
-                    <GoogleLogin />
-                </div>
-            </form>
+            <h2 className="mb-5">Đăng nhập</h2>
+            <div className="lp-button-group">
+                <GoogleLogin/>
+            </div>
         </div>
     )
 }
