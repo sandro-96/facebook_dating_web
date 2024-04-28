71848af3208c3c74788fcaa23fdb040e397696ab
sandro-96 <154776732+sandro-96@users.noreply.github.com>
2024-04-28 21:38:32 +0700

    fix bugs

--- src/components/Login/GoogleLogin/index.js
+++ src/components/Login/GoogleLogin/index.js
@@ -4,14 +4,15 @@ import {useTranslation} from "react-i18next";
 
 const GoogleLogin = props => {
     const { t } = useTranslation();
-  return (
-      <div className="lp-btn">
-          <a className="btn" href={`http://localhost:8080/oauth2/authorization/google`} role="button">
-              <img width="19px" className="m-auto" alt="Google sign-in" src={LogoGoogle}/>
-              {t('login.withGG')}
-          </a>
-      </div>
-  );
+    const authUrl = `${process.env.REACT_APP_API_BASE_URL}/oauth2/authorization/google`;
+    return (
+        <div className="lp-btn">
+            <a className="btn" href={authUrl} role="button">
+                <img width="19px" className="m-auto" alt="Google sign-in" src={LogoGoogle}/>
+                {t('login.withGG')}
+            </a>
+        </div>
+    );
 };
 
 GoogleLogin.propTypes = {};
