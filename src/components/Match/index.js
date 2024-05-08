d53c10b1ce668ee443410b86c5ec9c8fdb622901
sandro-96 <154776732+sandro-96@users.noreply.github.com>
2024-05-08 23:24:33 +0700

    fix bugs

--- src/components/Match/index.js
+++ src/components/Match/index.js
@@ -1,5 +1,5 @@
 import "./index.scss"
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {faHeart, faFilter, faBoltLightning} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import axios from "axios";
@@ -7,16 +7,20 @@ import DateUtils from "../Utils/DateUtils";
 import {useNavigate} from "react-router-dom";
 import Avatar from "../Avatar";
 import {useTranslation} from "react-i18next";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const MAX_LIKE_DAY = 10
+const PAGE_SIZE = 30;
 
 export const Match = () => {
     const [users, setUsers] = useState([])
     const navigate = useNavigate()
     const [liked, setLiked] = useState(MAX_LIKE_DAY)
     const { t } = useTranslation();
+    const [page, setPage] = useState(0);
+    const [totalPage, setTotalPage] = useState(0);
     useEffect(() => {
-        loadData()
+        loadData(page)
         getLikedCount()
     }, []);
 
@@ -26,9 +30,13 @@ export const Match = () => {
         })
     }
 
-    const loadData = () => {
-        axios.get(`users/list`).then(res => {
-            res.data && setUsers(res.data)
+    const loadData = (page) => {
+        axios.get(`users/list?page=${page}&size=${PAGE_SIZE}`).then(response => {
+            console.log(response.data)
+            if (response && response.data) {
+                setUsers(oldMessages => [...oldMessages, ...response.data.content]);
+                setTotalPage(response.data.totalPages);
+            }
         })
     }
     const handleChoose = (id) => {
@@ -40,45 +48,72 @@ export const Match = () => {
             setUsers(users.filter(value1 => value1.key !== id))
         })
     }
+    const fetchMoreData = () => {
+        if (page < totalPage - 1) {
+            setPage(page + 1); // increment page number
+            loadData(page + 1);
+        }
+    }
+
+    const hasMore = () => {
+        return page < totalPage - 1;
+    }
 
     return (
         users &&
         <div className="match-wrap">
-            <div className='mb-4 d-flex align-items-center'>
-                <div style={{width: 67}}></div>
-                <h2 className='flex-grow-1'>{t('match.title')}</h2>
-                <div className='btn-filter' onClick={event => navigate('/match/filter')}>
-                    <span>{t('match.filter')}</span>
-                    <FontAwesomeIcon icon={faFilter} size="xl" style={{color: "#e3e3e3"}}/>
+            <div className="match-header">
+                <div className='mb-4 d-flex align-items-center'>
+                    <div style={{width: 67}}></div>
+                    <h2 className='flex-grow-1'>{t('match.title')}</h2>
+                    <div className='btn-filter' onClick={event => navigate('/match/filter')}>
+                        <span>{t('match.filter')}</span>
+                        <FontAwesomeIcon icon={faFilter} size="xl" style={{color: "#e3e3e3"}}/>
+                    </div>
+                </div>
+                <div className="bolt-lightning">
+                    <FontAwesomeIcon icon={faBoltLightning} size="lg" style={{color: '#ff5050'}}/>
+                    <span>{liked}</span>
                 </div>
             </div>
-            <div className="bolt-lightning">
-                <FontAwesomeIcon icon={faBoltLightning} size="lg" style={{color: '#ff5050'}}/>
-                <span>{liked}</span>
-            </div>
-            {
-                users.map((value, index) => (
-                    <div className="d-flex" key={`person_${index}`}>
-                        <div key={`match-item_${index}`} className={`match-item ${value.gender ? value.gender : 'other'} gap-1`}>
-                            <Avatar imgKey={value.avatar} genderKey={value.gender} sizeKey={48}></Avatar>
-                            <div className="user-info">
-                                <div className='fs-5'>
-                                    <div className='ellipsis'>
-                                        {value.username}{value.birthYear > 0 &&
-                                        <span>, {DateUtils.calculateOlds(value.birthYear)}</span>}
+            <div className="content-wrap" id="scrollableDiv">
+                <InfiniteScroll
+                    dataLength={users.length}
+                    next={fetchMoreData}
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column'
+                    }}
+                    hasMore={hasMore()}
+                    loader={<span>{t('chat.loading')}</span>}
+                    scrollableTarget="scrollableDiv"
+                >
+                    {
+                        users.map((value, index) => (
+                            <div className="d-flex" key={`person_${index}`}>
+                                <div key={`match-item_${index}`}
+                                     className={`match-item ${value.gender ? value.gender : 'other'} gap-1`}>
+                                    <Avatar imgKey={value.avatar} genderKey={value.gender} sizeKey={48}></Avatar>
+                                    <div className="user-info">
+                                        <div className='fs-5'>
+                                            <div className='ellipsis'>
+                                                {value.username}{value.birthYear > 0 &&
+                                                <span>, {DateUtils.calculateOlds(value.birthYear)}</span>}
+                                            </div>
+                                        </div>
+                                        <div className='fw-normal bio'>{value.bio}</div>
                                     </div>
                                 </div>
-                                <div className='fw-normal bio'>{value.bio}</div>
+                                <div className="heart-icon" onClick={() => {
+                                    handleChoose(value.key)
+                                }}>
+                                    <FontAwesomeIcon icon={faHeart} size="2xl" style={{color: '#e3e3e3'}}/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="heart-icon" onClick={() => {
-                            handleChoose(value.key)
-                        }}>
-                            <FontAwesomeIcon icon={faHeart} size="2xl" style={{color: '#e3e3e3'}}/>
-                        </div>
-                    </div>
-                ))
-            }
+                        ))
+                    }
+                </InfiniteScroll>
+            </div>
         </div>
     )
 }
